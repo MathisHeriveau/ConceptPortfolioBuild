8bfb666632a4890da7353024c471446e541db0f7
HERIVEAU Mathis <mheriveau@cdg33.fr>
2024-05-02 09:38:24 +0200

    Concept de page de garde fini

--- characterControls.js
+++ characterControls.js
@@ -1,85 +1,341 @@
-// Créez une classe CharacterControls pour gérer le mouvement du personnage
 class CharacterControls {
-    constructor(model, mixer, camera) {
+    constructor(model, mixer, camera, gift) {
         this.model = model;
-        this.mixer = mixer;;
+        this.gift = gift;
+        this.mixer = mixer;
         this.camera = camera;
-        this.moveDirection = new THREE.Vector3(); // Direction de déplacement
-        this.movementSpeed = 0.1; // Vitesse de déplacement
-        this.currentAction = ''; // Action actuellement jouée
-        this.setupEventListeners(); // Mettre en place les écouteurs d'événements
+        this.moveDirection = new THREE.Vector3(); 
+        this.movementSpeed = 0.1; 
+        this.currentAngle = THREE.MathUtils.radToDeg(Math.atan2(this.camera.position.x - this.model.position.x, this.camera.position.z - this.model.position.z));
+        this.setupEventListeners(); 
+        this.touchePressed = {
+            up: false,
+            down: false,
+            left: false,
+            right: false
+        };
+        this.delta = 0;
+        this.model.rotation.y = THREE.MathUtils.degToRad(this.currentAngle);
+        this.timer = 0;
+        this.timerMax = Math.floor(Math.random() * (10 - 5 + 1)) + 4;
+
+        setInterval(() => {
+            this.timer += 1;
+            if (this.timer >= this.timerMax) {
+                console.log("Random animation : " + this.timer);
+                this.timer = 0;
+                this.timerMax = Math.floor(Math.random() * (10 - 5 + 1)) + 7;
+                this.doRandomAnimation();
+            }
+        }, 1000);
+        
+
+        // On joue la 2e animation
+        this.mixer.clipAction(this.gift.animations[1]).play();
     }
 
-    // Met en place les écouteurs d'événements pour les touches de direction
     setupEventListeners() {
         document.addEventListener('keydown', (event) => {
+            this.timer = 0;
+            if (this.mixer && (event.key === 'ArrowUp' || event.key === 'ArrowDown' || event.key === 'ArrowLeft' || event.key === 'ArrowRight')) {
+                this.mixer.clipAction(this.gift.animations[6]).play();
+                this.stopAllAnimations(6);
+            }
             switch (event.key) {
                 case 'ArrowUp':
-                    this.moveDirection.z = -1; // Avancer vers l'avant
+                    this.moveDirection.z = -1; 
+                    this.touchePressed.up = true;
                     break;
                 case 'ArrowDown':
-                    this.moveDirection.z = 1; // Avancer vers l'arrière
+                    this.moveDirection.z = 1; 
+                    this.touchePressed.down = true;
                     break;
                 case 'ArrowLeft':
-                    this.moveDirection.x = -1; // Déplacer vers la gauche
+                    this.moveDirection.x = -1; 
+                    this.touchePressed.left = true;
                     break;
                 case 'ArrowRight':
-                    this.moveDirection.x = 1; // Déplacer vers la droite
+                    this.moveDirection.x = 1; 
+                    this.touchePressed.right = true;
                     break;
             }
         });
-
+    
         document.addEventListener('keyup', (event) => {
+            this.timer = 0;
+            switch (event.key) {
+                case 'ArrowUp':
+                    this.touchePressed.up = false;
+                    break;
+                case 'ArrowDown':
+                    this.touchePressed.down = false;
+                    break;
+                case 'ArrowLeft':
+                    this.touchePressed.left = false;
+                    break;
+                case 'ArrowRight':
+                    this.touchePressed.right = false;
+                    break;
+            }
+
+            if (this.mixer) {
+                if (!this.touchePressed.up && !this.touchePressed.down && !this.touchePressed.left && !this.touchePressed.right) {
+                    this.mixer.clipAction(this.gift.animations[1]).play();
+                    this.stopAllAnimations(1);
+                }
+            }
             switch (event.key) {
                 case 'ArrowUp':
                 case 'ArrowDown':
-                    this.moveDirection.z = 0; // Arrêter le mouvement avant/arrière lorsque la touche est relâchée
+                    this.moveDirection.z = 0; 
                     break;
                 case 'ArrowLeft':
                 case 'ArrowRight':
-                    this.moveDirection.x = 0; // Arrêter le mouvement gauche/droite lorsque la touche est relâchée
+                    this.moveDirection.x = 0; 
                     break;
             }
         });
     }
+
 
     update(delta) {
         if (this.mixer) {
             this.mixer.update(delta);
+            this.delta = delta;
         }
     
-        // Mettre à jour le mouvement du modèle en fonction de la direction de déplacement
         this.model.position.x += this.moveDirection.x * this.movementSpeed;
         this.model.position.z += this.moveDirection.z * this.movementSpeed;
     
-        // Calculer l'angle de rotation par rapport à la direction actuelle du personnage
-        let targetAngle = Math.atan2(this.moveDirection.x, this.moveDirection.z);
+        if (this.moveDirection.x !== 0 || this.moveDirection.z !== 0) {
+            let targetAngle = Math.atan2(this.moveDirection.x, this.moveDirection.z);
+            targetAngle = THREE.MathUtils.radToDeg(targetAngle);
+            
+            // Normalize targetAngle to be within [0, 360] range
+            targetAngle = (targetAngle + 360) % 360;
+            
+            // Get current angle within [0, 360] range
+            let currentAngle = THREE.MathUtils.radToDeg(this.model.rotation.y);
+            currentAngle = (currentAngle + 360) % 360;
     
-        // Convertir l'angle en degrés
-        targetAngle = THREE.MathUtils.radToDeg(targetAngle);
+            // Calculate angle difference
+            let angleDiff = targetAngle - currentAngle;
+            angleDiff = THREE.MathUtils.clamp(angleDiff, -180, 180);
     
-        // Limiter l'angle entre -180 et 180 degrés pour éviter des rotations excessives
-        targetAngle = THREE.MathUtils.clamp(targetAngle, -180, 180);
+            // Adjust rotationAmount based on angle difference
+            const rotationSpeed = 360;
+            let rotationAmount = rotationSpeed * delta;
     
-        // Calculer la différence d'angle entre la direction actuelle et la nouvelle direction
-        let angleDiff = targetAngle - THREE.MathUtils.radToDeg(this.model.rotation.z);
+            if (Math.abs(angleDiff) < rotationAmount) {
+                rotationAmount = Math.abs(angleDiff);
+            }
     
-        // Limiter la différence d'angle entre -180 et 180 degrés pour obtenir la rotation la plus courte
+            // Update rotation direction
+            if (angleDiff < 0) {
+                rotationAmount *= -1;
+            }
+    
+            // Update model's rotation
+            this.model.rotation.y += THREE.MathUtils.degToRad(rotationAmount);
+    
+            // Update current angle
+            currentAngle += rotationAmount;
+            currentAngle = (currentAngle + 360) % 360;
+            this.currentAngle = THREE.MathUtils.degToRad(currentAngle);
+        }
+
+
+        console.log(this.isOutOfWindow());
+    }
+    
+    
+    
+    
+    
+
+    updateCurrentAngle() {
+        const angle = Math.atan2(-this.moveDirection.x, -this.moveDirection.z);
+        this.currentAngle = THREE.MathUtils.radToDeg(angle + Math.PI);
+    }
+
+    goToAngleSmooth(targetAngle) {
+        let angleDiff = targetAngle - THREE.MathUtils.radToDeg(this.model.rotation.y);
         angleDiff = THREE.MathUtils.clamp(angleDiff, -180, 180);
     
-        // Définir la vitesse de rotation pour obtenir une transition fluide
-        const rotationSpeed = 360; // Degrés par seconde
+        if (angleDiff > 180) {
+            angleDiff -= 360;
+        } else if (angleDiff < -180) {
+            angleDiff += 360;
+        }
     
-        // Interpoler progressivement l'angle actuel vers l'angle cible
-        let rotationAmount = rotationSpeed * delta;
+        const rotationSpeed = 360; 
+        let rotationAmount = rotationSpeed * this.delta;
+    
         if (Math.abs(angleDiff) < rotationAmount) {
             rotationAmount = Math.abs(angleDiff);
         }
+    
         if (angleDiff < 0) {
             rotationAmount *= -1;
         }
     
-        // Appliquer la rotation au modèle autour de l'axe Y avec l'angle ajusté
-        this.model.rotation.z += THREE.MathUtils.degToRad(rotationAmount);
+        const targetAngleRad = THREE.MathUtils.degToRad(targetAngle);
+    
+        var goToAngle = setInterval(() => {
+            var rotationYModulo = this.model.rotation.y % (2 * Math.PI);
+            var targetAngleModulo = targetAngleRad % (2 * Math.PI);
+            if (Math.abs(rotationYModulo - targetAngleModulo) < 0.1) {
+                clearInterval(goToAngle);
+            } else {
+                this.model.rotation.y += THREE.MathUtils.degToRad(rotationAmount);
+            }
+
+        }, 20);
+    
+        this.currentAngle = this.model.rotation.y;    
     }
+    
+
+    doHello() {
+        if (this.mixer) {
+            this.timer = 0;
+            // On regarde l'angle entre la camera et le modèle
+            let angle = Math.atan2(this.camera.position.x - this.model.position.x, this.camera.position.z - this.model.position.z);
+            angle = THREE.MathUtils.radToDeg(angle);
+            this.goToAngleSmooth(angle);
+
+            this.mixer.clipAction(this.gift.animations[3]).play();
+            this.stopAllAnimations(3);
+
+            // On recupere la durée de l'animation
+            let duration = this.gift.animations[3].duration * 1000;
+            this.timerMax += duration / 1000;
+    
+            
+            setTimeout(() => {
+                this.mixer.clipAction(this.gift.animations[1]).play();
+
+                this.mixer.clipAction(this.gift.animations[3]).stop();
+                
+            }, 2500);
+        }
+    }
+
+    doRandomAnimation(){
+        if (this.mixer) {
+            // On choisi un numero random dans la liste des animations
+            let listeAnimations = [3,5];
+            let random = Math.floor(Math.random() * listeAnimations.length);
+            let animation = listeAnimations[random];
+
+            let angle = Math.atan2(this.camera.position.x - this.model.position.x, this.camera.position.z - this.model.position.z);
+            angle = THREE.MathUtils.radToDeg(angle);
+            this.goToAngleSmooth(angle);
+
+            this.mixer.clipAction(this.gift.animations[animation]).play();
+            this.stopAllAnimations(animation);
+
+            // On recupere la durée de l'animation
+            let duration = this.gift.animations[animation].duration * 1000;
+            this.timerMax += duration / 1000;
+
+
+            setTimeout(() => {
+                this.mixer.clipAction(this.gift.animations[1]).play();
+                this.mixer.clipAction(this.gift.animations[animation]).stop();
+            }, 2500);
+        }
+    }
+
+    stopAllAnimations(j) {
+        if (this.mixer) {
+            for (let i = 0; i < this.gift.animations.length; i++) {
+                // Si i == j on ne stop pas l'animation
+                if (i !== j) {
+                    this.mixer.clipAction(this.gift.animations[i]).stop();
+                }
+            }
+        }
+    }
+
+    isOutOfWindow() {
+        var posz = this.model.position.z;
+
+        if (this.model.position.z < -30) {
+            // si window.innerWidth < 1000 on tp en 0,0,0 sinon on tp en 7,0,5
+            if (window.innerWidth < 1000) {
+                this.model.position.set(0, 0, 0);
+            } else {
+                this.model.position.set(7, 0, 5);
+            }
+            return false;
+        }
+
+        console.log("Position z : " + posz);
+        // Calcul de f en utilisant des intervalles
+        var f = this.calculateF(posz);
+
+        // Vérifier si la position x est en dehors de la fenêtre en fonction de f
+        if (this.model.position.x > (window.innerWidth / f) || this.model.position.x < - (window.innerWidth / f)) {
+            return true;
+        }
+        return false;
+    }
+
+    calculateF(posz) {
+        var intervals = [-25, -23.8, -17.8, -11.8, -5.8, 0.2, 5.2, 10.2];
+        var values = [30,28, 34, 40, 50, 70, 110, 150];
+        var newIntervals = [];
+        var newValues = [];
+
+        for (var i = 0; i < intervals.length - 1; i++) {
+            var currentInterval = intervals[i];
+            var nextInterval = intervals[i + 1];
+            var currentValues = values[i];
+            var nextValues = values[i + 1];
+
+            newIntervals.push(currentInterval);
+
+            // Calculer la moyenne entre les intervalles
+            var averageInterval = (currentInterval + nextInterval) / 2;
+            newIntervals.push(averageInterval);
+
+            newValues.push(currentValues);
+
+            // Calculer la moyenne entre les valeurs
+            var averageValue = (currentValues + nextValues) / 2;
+            newValues.push(averageValue);
+        }
+
+        newIntervals.push(intervals[intervals.length - 1]);
+        newValues.push(values[values.length - 1]);
+
+        // Utiliser les nouvelles valeurs interpolées
+        for (var i = 0; i < newIntervals.length; i++) {
+            if (posz < newIntervals[i]) {
+                return newValues[i];
+            }
+        }
+
+        return 100; // Valeur par défaut si aucune correspondance n'est trouvée
+    }
+    
+
+    /*
+justWaiting // 0 : bug
+justWaiting2 // 1 : Trés bien pour les attentes
+T-Pose
+WaitingHappy // 3 : Il saute un peu mais il est content
+WaitingHot // 4 : bug
+WaitinSad // 5 : Tres direct mais ca va
+walkInPlace // 6 : Il marche sur place 
+walkInPlace // 7 : bug
+justWaiting  // 8 : bug
+WaitingHappy // 9 : bug
+WaitingHappy // 10 : bug
+JustWaiting2 // 11 : bug
+WaitinSad // 12 : bug
+    */
+
+    
 }
--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function init() {
     renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(container.clientWidth, container.clientHeight);
     container.appendChild(renderer.domElement);
+    renderer.shadowMap.enabled = true;
 
     window.addEventListener('resize', () => {
         renderer.setSize(container.clientWidth, container.clientHeight);
@@ -22,70 +23,93 @@ function init() {
     });
 
     camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-    camera.position.set(0, 6, 15);
+    camera.position.set(0, 5, 15);
 
     //set up controls
     // controls = new THREE.OrbitControls(camera, renderer.domElement);
     // controls.enableDamping = true;
-    //set up lights : hemispherical light
 
+    //set up lights : hemispherical light
     hemiLight = new THREE.HemisphereLight();
     scene.add(hemiLight);
 
     //set up lights : spot lights
 
     spotLight = new THREE.SpotLight(0xffa95c, 18);
-    //spotLight.castShadow = true;
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize.width = 2048;
+    spotLight.shadow.mapSize.height = 2048;
+    spotLight.shadow.radius = 3;
+
     spotLight.position.set(-10, -10, -10);
+    // On crée un helper pour voir la direction de la lumière
+    const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+    scene.add(spotLightHelper);
+
     scene.add(spotLight);
 
     //tone mapping
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.toneMappingExposure = 1.0;
 
-    //model upload
-    renderer.shadowMap.enabled = true;
 
 
     // On ajoute un sol cadrillé
-    grid= new THREE.GridHelper(100, 100, 0x888888, 0x444444);
+
+    /// On créé une vrai grille qui va nous servir de sol pour l'ombre
+    var planeGeometry = new THREE.PlaneGeometry(100, 100, 100, 100);
+    var planeMaterial = new THREE.ShadowMaterial();
+    planeMaterial.opacity = 0.5;
+    var grid = new THREE.Mesh(planeGeometry, planeMaterial);
+    grid.receiveShadow = true;
+    grid.rotation.x = -Math.PI / 2;
     scene.add(grid);
 
-    new THREE.GLTFLoader().load(`model/gir/scene.gltf`, result => {
-        model = result.scene.children[0];
-        model.position.set(0, 0, 0);
-        model.scale.set(0.3, 0.3, 0.3);
+    new THREE.GLTFLoader().load(`model/gir animer/Mon petit perso marche.glb`,  function ( gltf ){
+        model = gltf.scene;
+        if (container.clientWidth < 768) {
+            model.position.set(0, 0, 0);
+        } else {
+            model.position.set(7, 0, 5);
+        }
+        model.scale.set(2, 2, 2);
+        // rotate de 90°
+        model.rotation.y = - Math.PI / 1.5;
 
         // On créé un cube pour la hitbox qui prend toute la hauteur du modèle
-        var geometry = new THREE.BoxGeometry(model.scale.x * 3, model.scale.y *8, model.scale.z *3);
+        var geometry = new THREE.BoxGeometry(model.scale.x * 1, model.scale.y *1, model.scale.z *1);
         // color in transparent for the cube
         var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 0 });
         hitBox = new THREE.Mesh(geometry, material);
-        hitBox.position.set(0, model.scale.y *3, 0);
+        hitBox.position.set(0, 0, 0);
         hitBox.name = "hitBox";
+        hitBox.visible = false;
 
         scene.add(hitBox);
         scene.add(model);
 
+        model.traverse((child) => {
+            if (child.isMesh) {
+                child.castShadow = true;
+            }
+        });
 
-        
-        // On se positionne sur le modèle
-        camera.lookAt(model.position);
+
     
         // Si il a une animation, on la joue
-        if (result.animations.length > 0) {
+        if (gltf.animations.length > 0) {
             mixer = new THREE.AnimationMixer(model);
-            mixer.clipAction(result.animations[0]).play();
         } else {
             alert("No animation found");
         }
     
-        characterControls = new CharacterControls(model, mixer, camera);
+        characterControls = new CharacterControls(model, mixer, camera, gltf);
     
         animate();
     });
 
     document.addEventListener('mouseup', onDocumentMouseClick, false);
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
     
     
 }
@@ -101,7 +125,7 @@ function animate() {
     if (mixer) {
         mixer.update(0.01);
     }
-    hitBox.position.set(model.position.x, model.position.y + model.scale.y * 3, model.position.z);
+    hitBox.position.set(model.position.x, model.position.y + model.scale.y/2, model.position.z);
     characterControls.update(0.01);
 
     requestAnimationFrame(animate);
@@ -121,13 +145,9 @@ function onDocumentMouseClick(event) {
     var intersects = raycaster.intersectObjects(scene.children, true);
     if (intersects.length > 0) {
         if (INTERSECTED != intersects[0].object) {
-            // if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
             INTERSECTED = intersects[0].object;
-            // INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-            // INTERSECTED.material.emissive.setHex(0xff0000);
-            // Si l'objet cliqué est le modèle, on alerte
-            if (INTERSECTED && INTERSECTED.name == "Object_125") {
-                console.log("Model clicked");
+            if (INTERSECTED && INTERSECTED.name == "hitBox") {
+                characterControls.doHello();
             } else if (INTERSECTED == grid) {
                 console.log("Grid clicked");
             } else if (INTERSECTED != null) {
@@ -138,6 +158,31 @@ function onDocumentMouseClick(event) {
     } else {
         console.log("Nothing clicked");
 
+        INTERSECTED = null;
+    }
+}
+
+function onDocumentMouseMove(event) {
+    // Si la souris passe au dessus de hitbox on cursor pointer
+    event.preventDefault();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    var raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    if (intersects.length > 0) {
+        if (INTERSECTED != intersects[0].object) {
+            INTERSECTED = intersects[0].object;
+            if (INTERSECTED && INTERSECTED.name == "hitBox") {
+                document.body.style.cursor = "pointer";
+            } else {
+                document.body.style.cursor = "default";
+            }
+        }
+        INTERSECTED = null;
+    } else {
+        document.body.style.cursor = "default";
         INTERSECTED = null;
     }
 }
@@ -158,3 +203,4 @@ function addTextToScene (textAEcrire, position)  {
         scene.add( text );
     });
 }
+
